3581f73480213d7c9ba51523fd90936dd85b8148
Su Wei <561496211@qq.com>
2017-08-29 14:32:16 +0800

    基于scrapy的糗百爬虫

--- qiubai/qiubai/spiders/qiubai_spider.py
+++ qiubai/qiubai/spiders/qiubai_spider.py
@@ -3,18 +3,18 @@ import scrapy,sys
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor  
 from bs4 import BeautifulSoup  
 
-class QiubaiSpider(scrapy.spiders.BaseSpider):
+class QiubaiSpider(scrapy.spiders.Spider):
 	name = "qiubai"
 	allowed_domains = ["qiushibaike.com"]
 	start_urls = [
 		"http://www.qiushibaike.com/hot/page/",
-	]
+	]	
 	def parse(self, response):
 		print "\n开始载入糗百段子(按q退出):\n".decode('utf-8')
 		data = response.body
 		soup = BeautifulSoup(data, "lxml") 
-		contents = soup.find_all("div","article")
-		
+		contents = soup.find_all("div","article")	
+		#获取每条段子的内容、作者和点赞数
 		for content in contents:
 			cont = content.span.get_text().encode("GBK", "ignore");
 			auth = content.h2.get_text().replace("\n","").encode("GBK", "ignore");
@@ -23,10 +23,10 @@ class QiubaiSpider(scrapy.spiders.BaseSpider):
 			print cont,"-"*40
 			input = raw_input(' ') 
 			if input == "q" or input == "Q" :
-				sys.exit()
-				
+				sys.exit()		
+		#爬取下一页内容
 		next_page = 'http://qiushibaike.com'+soup.find("span",'next').parent['href']
-		print "next_url:",next_page
+		#print "next_url:",next_page
 		yield scrapy.Request(next_page, callback=self.parse)
 		
 		
