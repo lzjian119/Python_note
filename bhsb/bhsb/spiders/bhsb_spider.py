c483ecd8591f4808bd10206d74f8145a9667d81f
sweet <561496211@qq.com>
2018-06-09 22:47:53 +0800

    拉取bhsb gif图片 v1.0

--- bhsb/bhsb/spiders/bhsb_spider.py
+++ bhsb/bhsb/spiders/bhsb_spider.py
@@ -10,21 +10,23 @@ sys.setdefaultencoding( "utf-8" )
 
 class BhsbSpider(scrapy.spiders.Spider):
 	name = "bhsb"
-	allowed_domains = ["bohaishibei.com"]
-	start_urls = [
-		"https://bohaishibei.com/post/category/main/"	
-	]
-	# for i in range(2,5):
-		# su="https://bohaishibei.com/post/category/main/page/"+str(i)
-		# start_urls.append(su)
+	allowed_domains = ["bh.sb"]
+	start_urls = []
+	# start_urls = [
+		# "https://bh.sb/post/category/main/"	
+	# ]
+	for i in range(1,5):
+		su="https://bh.sb/post/category/main/page/"+str(i)
+		start_urls.append(su)
 
 	def parse(self, response):		
 		#获取当前页所有二级页面url
 		data = response.body
 		soup = BeautifulSoup(data, "lxml") 
-		urls = soup.find_all(href=re.compile("bohaishibei"),target='_blank')
+		urls = response.xpath('//*[@class="excerpt excerpt-one"]/header/h2/a/@href').extract()
 		for url in urls:
-			yield scrapy.Request(url['href'], callback=self.parse_content)
+			yield scrapy.Request(url, callback=self.parse_content)
+			
 	
 	#二级页面内容获取并保存到item	
 	def parse_content(self, response):
